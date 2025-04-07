bb4e560c00cdc1450051dd8a20b9059d9796b441
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-04-06 21:32:56 -0300

    updated refatoração aula final

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,44 @@
 window.onloadedPersonagensEBotoes = function onloadedPersonagensEBotoes() {
   try {
     const listaBotoes = document.querySelectorAll(".botao");
-		const listaPersonagens = document.querySelectorAll(".personagem");
+    const listaPersonagens = document.querySelectorAll(".personagem");
 
     console.log(listaBotoes);
 
-    listaBotoes.forEach((botao,i) => {
+    listaBotoes.forEach((botao, i) => {
       botao.addEventListener("click", () => {
-          const botaoSelecionado = document.querySelector(".botao.selecionado");
-					const personSelecionado = document.querySelector(".personagem.selecionado");
-					const personagem = listaPersonagens[i];
+        removerBotaoSelecao();
+        selecionarBotao(botao);
 
-          botaoSelecionado.classList.remove("selecionado");
-          botao.classList.add("selecionado");
-
-					personSelecionado.classList.remove("selecionado");
-					personagem.classList.add("selecionado");
+        removerSelecaoPersonagem();
+        selecionarPersonagem(i);
       });
     });
+
+    function removerBotaoSelecao() {
+      const botaoSelecionado = document.querySelector(".botao.selecionado");
+      if (botaoSelecionado) {
+        botaoSelecionado.classList.remove("selecionado");
+      }
+    }
+
+    function selecionarBotao(botao) {
+      botao.classList.add("selecionado");
+    }
+
+    function removerSelecaoPersonagem() {
+      const personSelecionado = document.querySelector(
+        ".personagem.selecionado"
+      );
+      if (personSelecionado) {
+        personSelecionado.classList.remove("selecionado");
+      }
+    }
+
+    function selecionarPersonagem(indice) {
+      const personagem = listaPersonagens[indice];
+      personagem.classList.add("selecionado");
+    }
   } catch (error) {
     alert("ERRO FATAL! REPORTE AO ADMINISTRADOR DO SITE. \n Error:" + error);
   }
